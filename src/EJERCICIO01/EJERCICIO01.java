4d887a19208b7d1db5bc9e709e537b863037c3b6
diurno <diurno@192.168.0.108>
2024-11-11 10:22:29 +0100

    ejercicios tema 1(lunes)

---
+++ src/EJERCICIO01/EJERCICIO01.java
@@ -0,0 +1,14 @@
+package EJERCICIO01;
+
+public class EJERCICIO01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("voy a aprender java");
+		
+		System.out.println("cueste lo que cueste");
+ 
+
+	}
+
+}
---
+++ src/EJERCICIO01/tercerejercicio.java
@@ -0,0 +1,29 @@
+package EJERCICIO01;
+
+import java.util.Scanner;
+
+public class tercerejercicio {
+
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		
+		byte hermanos;"4"
+		int nota; "9"
+		int población = "Extremadura";
+		int provincia = "Cáceres";
+		byte pasajeros = "120";
+
+		System.out.println( "Introduzca el numero de hermanos");
+		hermanos = entrada.next();
+		System.out.println("Introduzca tu nota final en esta clase");
+		nota= entrada.next()
+		System.out.println("Introduzca tu población");
+		población= entrada.next();
+		
+		
+		
+		
+	}
+
+}
