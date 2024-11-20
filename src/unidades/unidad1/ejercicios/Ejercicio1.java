46a4081180daca5141a3ea1cefdda2a720812f74
diurno <diurno@equipo-00>
2024-11-20 13:04:37 +0100

    miercoles

--- src/unidades/unidad1/ejercicios/Ejercicio1.java
+++ src/unidades/unidad1/ejercicios/Ejercicio1.java
@@ -3,9 +3,10 @@ package unidades.unidad1.ejercicios;
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-
 		
-		System.out.println("Ejercicio 1");
+		System.out.println("voy a aprender java");
+		System.out.println  ("cueste lo que cueste");
+		
 	}
 
 }
---
+++ src/unidades/unidad2/ejercicios/Ejemplo02.java
@@ -0,0 +1,56 @@
+package unidades.unidad2.ejercicios;
+
+import java.util.Scanner;
+
+public class Ejemplo02 {
+
+	public static void main(String[] args) {
+		  Scanner scanner = new Scanner(System.in);
+
+		  System.out.print("¿Cuántos hermanos tienes? ");
+	  
+	        int cantidadHermanos = scanner.nextInt();
+	        
+	      
+	        System.out.println("Tienes " + cantidadHermanos + " hermano(s).");
+	        
+	        System.out.println("¿cual es tu nota final? ");
+	        int Notafinal= scanner.nextInt();
+	        
+	        System.out.println("Tienes " + Notafinal +" de nota final");
+	        
+	        System.out.println ("¿En que provincia vives?");
+	
+	        String provincia= scanner.next();
+	        System.out.println("Vivo en " + provincia );
+	
+	        System.out.println("¿en qué pueblo vives? ");
+	        String  pueblo= scanner.next();
+	        System.out.println("Vivo en " + pueblo );
+	        
+	        System.out.print("¿Cuantos pasajeros hay en un bus? ");
+	        int cantidadPasajeros= scanner.nextInt();
+	        System.out.println("Hay " + cantidadPasajeros + " pasajeros en un bus ");
+	        
+	        System.out.println("¿Con cuantos puntos gana un jugador de baloncesto  ? ");
+	        int numPuntos = scanner.nextInt();
+	        System.out.println("Un jugador gana " + numPuntos + " en baloncesto ");
+	        
+	        System.out.println("¿Cuanto vale este artículo ? ");
+	        double precio$  = scanner.nextDouble();
+	        System.out.println("este artículo vale " + precio$ +"€");
+	        
+	        System.out.println("¿en qué año subió el hombre a la luna  ? ");
+	        int año = scanner.nextInt();
+	        System.out.println("el hombre subió a la luna en " + año );
+	        
+	        
+	        
+	        
+	  
+	        
+	        scanner.close();
+
+	}
+
+}
